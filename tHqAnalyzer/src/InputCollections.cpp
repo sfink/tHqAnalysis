92cd0cc2addf06f3c5b9f088284ed54ece5a1d4c
Simon Fink <simon.fink@kit.edu>
2015-04-09 11:27:09 +0200

    fixed missing std:: and wrong identifier

--- tHqAnalyzer/src/InputCollections.cpp
+++ tHqAnalyzer/src/InputCollections.cpp
@@ -1,5 +1,5 @@
 #include "../interface/InputCollections.hpp"
 void InputCollections::Dump(){
 
-  std::cout << "Implement Dumping function." << endl;
+  std::cout << "Implement Dumping function." << std::endl;
 }
