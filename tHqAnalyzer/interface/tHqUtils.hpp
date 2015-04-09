975aa8259f3b130975088e421356635f425b28ac
Simon Fink <simon.fink@kit.edu>
2015-04-09 11:18:35 +0200

    adapted Utils

---
+++ tHqAnalyzer/interface/tHqUtils.hpp
@@ -0,0 +1,159 @@
+#ifndef THQANALYSIS_THQANALYZER_BOOSTEDUTILS_HPP
+#define THQANALYSIS_THQANALYZER_BOOSTEDUTILS_HPP
+
+#include <vector>
+
+#include "TMath.h"
+#include "TVector.h"
+#include "TLorentzVector.h"
+
+#include "Math/VectorUtil.h"
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/Event.h"
+
+#include "DataFormats/FWLite/interface/Event.h"
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
+#include "DataFormats/PatCandidates/interface/Electron.h"
+#include "DataFormats/PatCandidates/interface/Jet.h"
+#include "DataFormats/PatCandidates/interface/MET.h"
+#include "tHqAnalysis/tHqObjects/interface/SubFilterJet.h"
+#include "tHqAnalysis/tHqObjects/interface/HEPTopJet.h"
+
+class tHqUtils{
+  
+  public:
+    
+    static std::string GetAnalyzerPath();
+    
+    static TLorentzVector GetTLorentzVector(const math::XYZTLorentzVector& vec);
+    static math::XYZTLorentzVector GetXYZTLorentzVector(const TLorentzVector& vec);
+    
+    static bool FirstIsLarger(float val1,float val2);
+    static bool FirstIsHarder(math::XYZTLorentzVector vec1,math::XYZTLorentzVector vec2);
+    static bool FirstHasHigherCSV(pat::Jet jet1,pat::Jet jet2);
+    static bool FirstHasHigherCSVold(pat::Jet jet1,pat::Jet jet2);
+    static bool FirstJetIsHarder(pat::Jet jet1, pat::Jet jet2);
+    template<typename boostedJetType>
+    static bool FirstFatJetIsHarder(boostedJetType jet1, boostedJetType jet2){
+      return tHqUtils::FirstJetIsHarder(jet1.fatjet,jet2.fatjet);
+    }
+    
+    static float DeltaEta(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2);
+    static float DeltaEta(const pat::Jet& jet1,const pat::Jet& jet2);
+    static float DeltaPhi(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2);
+    static float DeltaPhi(const pat::Jet& jet1,const pat::Jet& jet2);
+    static float DeltaR(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2);
+    static float DeltaR(const pat::Jet& jet1,const pat::Jet& jet2);
+    static float DeltaKt(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2);
+    static float DeltaKt(const pat::Jet& jet1,const pat::Jet& jet2);
+    
+    static float CosThetaStar(const math::XYZTLorentzVector& vec1, const math::XYZTLorentzVector& vec2);
+    static float CosThetaCM(const math::XYZTLorentzVector& vec,const math::XYZTLorentzVector& boostVec);
+    
+    static std::vector<math::XYZTLorentzVector> GetGenParticleVecs(const std::vector<reco::GenParticle>& genParticles);
+    static bool MCContainsTTbar(const std::vector<reco::GenParticle>& genParticles);
+    static bool MCContainsST(const std::vector<reco::GenParticle>& genParticles);
+    static bool MCContainsHiggs(const std::vector<reco::GenParticle>& genParticles);
+    static void GetttHMCParticles(const std::vector<reco::GenParticle>& genParticles, std::vector<reco::GenParticle>& tophad, std::vector<reco::GenParticle>& whad, std::vector<reco::GenParticle>& bhad, std::vector<reco::GenParticle>& q1, std::vector<reco::GenParticle>& q2, std::vector<reco::GenParticle>& toplep, std::vector<reco::GenParticle>& wlep, std::vector<reco::GenParticle>& blep, std::vector<reco::GenParticle>& lep, std::vector<reco::GenParticle>& nu, reco::GenParticle& higgs, reco::GenParticle& b1, reco::GenParticle& b2);
+    static void GetttHMCVecs(const std::vector<reco::GenParticle>& genParticles, std::vector<math::XYZTLorentzVector>& tophadvecs, std::vector<math::XYZTLorentzVector>& whadvecs, std::vector<math::XYZTLorentzVector>& bhadvecs, std::vector<math::XYZTLorentzVector>& q1vecs, std::vector<math::XYZTLorentzVector>& q2vecs, std::vector<math::XYZTLorentzVector>& toplepvecs, std::vector<math::XYZTLorentzVector>& wlepvecs, std::vector<math::XYZTLorentzVector>& blepvecs, std::vector<math::XYZTLorentzVector>& lepvecs, std::vector<math::XYZTLorentzVector>& nuvecs, math::XYZTLorentzVector& higgsvec, math::XYZTLorentzVector& b1vec, math::XYZTLorentzVector& b2vec);
+    
+    static bool IsAnyTriggerBitFired(const std::vector<std::string>& targetTriggers, const edm::TriggerResults& triggerResults);
+    
+    static std::vector<math::XYZTLorentzVector> GetLepVecs(const std::vector<pat::Electron>& selectedElectrons, const std::vector<pat::Muon> selectedMuons);
+    static math::XYZTLorentzVector GetPrimLepVec(const std::vector<pat::Electron>& selectedElectrons, const std::vector<pat::Muon> selectedMuons);
+    
+  static void GetNuVecs(const math::XYZTLorentzVector& lepvec, const TVector2& metvec, math::XYZTLorentzVector& nu1, math::XYZTLorentzVector& nu2); // edit needed \\\\\\\\\\\\\\\\
+    
+    static std::vector<math::XYZTLorentzVector> GetJetVecs(const std::vector<pat::Jet>& jets);
+    
+    static bool PassesCSV(const pat::Jet& jet, const char workingPoint);
+    
+    static float GetClosestJetIDs(int& idJet1, int& idJet2, const std::vector<pat::Jet>& jets);
+    static float GetClosestLepJetID(int& idJet, const math::XYZTLorentzVector& lepVec, const std::vector<pat::Jet>& jets);
+    
+    static float GetJetAverageJetEtaMax(const std::vector<pat::Jet>& jets1, const std::vector<pat::Jet>& jets2);
+    
+    static void GetFoxWolframMoments(std::vector<math::XYZTLorentzVector> jetVecs, float &h0, float &h1, float &h2, float &h3, float &h4);
+    static void GetAplanaritySphericity(math::XYZTLorentzVector leptonVec, math::XYZTLorentzVector metVec, std::vector<math::XYZTLorentzVector> jetVecs, float &aplanarity, float &sphericity);
+    
+  template <typename T, typename S> static std::vector<T> RemoveOverlaps( const std::vector<S>&, const std::vector<T>& );
+  template <typename T, typename S> static T RemoveOverlap( const std::vector<S>&, const T& );
+  
+  private:
+  
+};
+
+
+template <typename PATObj1, typename PATObj2> 
+PATObj1 BoostedUtils::RemoveOverlap( const std::vector<PATObj2>& other, const PATObj1& unclean ){
+
+  unsigned int nSources1 = unclean.numberOfSourceCandidatePtrs();
+  bool hasOverlaps = false;
+
+  std::vector<reco::CandidatePtr> overlaps;
+
+  for( typename std::vector<PATObj2>::const_iterator iobj2 = other.begin(); iobj2!=other.end(); ++iobj2 ){
+
+    unsigned int nSources2 = iobj2->numberOfSourceCandidatePtrs();
+
+    for( unsigned int i1=0; i1<nSources1; i1++ ){
+      reco::CandidatePtr source1 = unclean.sourceCandidatePtr(i1);
+
+      if( !(source1.isNonnull() && source1.isAvailable()) ) continue;
+
+      for( unsigned int i2=0; i2<nSources2; i2++ ){
+	reco::CandidatePtr source2 = iobj2->sourceCandidatePtr(i2);
+
+	if( !(source2.isNonnull() && source2.isAvailable()) ) continue;
+
+	if( source1==source2 ){
+	  hasOverlaps = true;
+	  overlaps.push_back(source2);
+	}
+      }
+
+    }
+  }// end loop over iobj22
+
+
+  PATObj1 cleaned = unclean;
+  if( hasOverlaps ){
+    math::XYZTLorentzVector original = cleaned.p4();
+
+    for( int iOverlap=0; iOverlap<int(overlaps.size()); iOverlap++ ){
+
+      const reco::Candidate & cOverlap = *(overlaps[iOverlap]);
+      math::XYZTLorentzVector overlaper = cOverlap.p4();
+
+      original -= overlaper;
+    }
+
+    cleaned.setP4( original );
+  }
+
+  return cleaned;
+}
+
+
+template <typename PATObj1, typename PATObj2> 
+std::vector<PATObj1> BoostedUtils::RemoveOverlaps( const std::vector<PATObj2>& other, const std::vector<PATObj1>& unclean ){
+
+  std::vector<PATObj1> cleaned;
+  
+  for( typename std::vector<PATObj1>::const_iterator iobj1 = unclean.begin(); iobj1!=unclean.end(); ++iobj1 ){
+
+    PATObj1 myobj = (*iobj1);
+    PATObj1 clean = RemoveOverlap(other, myobj);
+
+    cleaned.push_back(clean);
+  }
+
+  return cleaned;
+}
+    
+
+
+#endif
---
+++ tHqAnalyzer/src/tHqUtils.cpp
@@ -0,0 +1,582 @@
+#include "tHqAnalysis/tHqAnalyzer/interface/tHqUtils.hpp"
+
+using namespace std;
+
+
+std::string tHqUtils::GetAnalyzerPath(){
+  char* cpath;
+  cpath = getenv("CMSSW_BASE");
+  std::string path = cpath;
+  
+  if (path.size() == 0){
+    cerr << "could not find path of exe" << endl;
+    return path;
+  }
+  else
+    return path+"/src/tHqAnalysis/tHqAnalyzer";
+}
+
+TLorentzVector tHqUtils::GetTLorentzVector(const math::XYZTLorentzVector& vec){
+  
+  TLorentzVector result(vec.Px(),vec.Py(),vec.Pz(),vec.E());
+  
+  return result;
+  
+}
+
+
+math::XYZTLorentzVector tHqUtils::GetXYZTLorentzVector(const TLorentzVector& vec){
+  
+  math::XYZTLorentzVector result(vec.Px(),vec.Py(),vec.Pz(),vec.E());
+  
+  return result;
+  
+}
+
+
+bool tHqUtils::FirstIsLarger(float val1,float val2){
+  return val1 > val2;
+}
+
+
+bool tHqUtils::FirstIsHarder(math::XYZTLorentzVector vec1,math::XYZTLorentzVector vec2){
+  return vec1.Pt()>vec2.Pt();
+}
+
+
+bool tHqUtils::FirstJetIsHarder(pat::Jet jet1, pat::Jet jet2){
+  return jet1.pt()>jet2.pt();
+}
+
+
+bool tHqUtils::FirstHasHigherCSV(pat::Jet jet1,pat::Jet jet2){
+  return jet1.bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags") > jet2.bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags");
+}
+
+bool tHqUtils::FirstHasHigherCSVold(pat::Jet jet1,pat::Jet jet2){
+  return jet1.bDiscriminator("combinedSecondaryVertexBJetTags") > jet2.bDiscriminator("combinedSecondaryVertexBJetTags");
+}
+
+
+float tHqUtils::DeltaEta(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2){
+  if(vec1.Pt()<0.001||vec2.Pt()<0.001) return -2;
+  
+  float deta = fabs(vec1.Eta()-vec2.Eta());
+  
+  return deta;
+}
+
+
+float tHqUtils::DeltaEta(const pat::Jet& jet1,const pat::Jet& jet2){
+  if(jet1.pt()<0.001||jet2.pt()<0.001) return -2;
+  
+  math::XYZTLorentzVector vec1 = jet1.p4();
+  math::XYZTLorentzVector vec2 = jet2.p4();
+  
+  float deta = fabs(vec1.Eta()-vec2.Eta());
+  
+  return deta;
+}
+
+
+float tHqUtils::DeltaPhi(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2){
+  if(vec1.Pt()<0.001||vec2.Pt()<0.001) return -2;
+  
+  float dphi = ROOT::Math::VectorUtil::DeltaPhi(vec1,vec2);
+  
+  return fabs(dphi);
+}
+
+
+float tHqUtils::DeltaPhi(const pat::Jet& jet1,const pat::Jet& jet2){
+  if(jet1.pt()<0.001||jet2.pt()<0.001) return -2;
+  
+  math::XYZTLorentzVector vec1 = jet1.p4();
+  math::XYZTLorentzVector vec2 = jet2.p4();
+  
+  float dphi = ROOT::Math::VectorUtil::DeltaPhi(vec1,vec2);
+  
+  return fabs(dphi);
+}
+
+
+float tHqUtils::DeltaR(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2){
+  if(vec1.Pt()<0.001||vec2.Pt()<0.001) return -2;
+  
+  float dr = ROOT::Math::VectorUtil::DeltaR(vec1,vec2);
+  
+  return dr;
+}
+
+
+float tHqUtils::DeltaR(const pat::Jet& jet1,const pat::Jet& jet2){
+  if(jet1.pt()<0.001||jet2.pt()<0.001) return -2;
+  
+  math::XYZTLorentzVector vec1 = jet1.p4();
+  math::XYZTLorentzVector vec2 = jet2.p4();
+  
+  float dr = ROOT::Math::VectorUtil::DeltaR(vec1,vec2);
+  
+  return dr;
+}
+
+
+float tHqUtils::DeltaKt(const math::XYZTLorentzVector& vec1,const math::XYZTLorentzVector& vec2){
+  if(vec1.Pt()<0.001 || vec2.Pt()<0.001) return -2;
+  
+  float dr = ROOT::Math::VectorUtil::DeltaR(vec1,vec2);
+  float ptmin=min(vec1.Pt(),vec2.Pt());
+  
+  return sqrt(dr*dr*ptmin*ptmin);
+}
+
+
+float tHqUtils::DeltaKt(const pat::Jet& jet1,const pat::Jet& jet2){
+  if(jet1.pt()<0.001||jet2.pt()<0.001) return -2;
+  
+  math::XYZTLorentzVector vec1 = jet1.p4();
+  math::XYZTLorentzVector vec2 = jet2.p4();
+  
+  float dr = ROOT::Math::VectorUtil::DeltaR(vec1,vec2);
+  float ptmin=min(vec1.Pt(),vec2.Pt());
+  
+  return sqrt(dr*dr*ptmin*ptmin);
+}
+
+
+float tHqUtils::CosThetaStar(const math::XYZTLorentzVector& vec1, const math::XYZTLorentzVector& vec2){
+  if(vec1.Pt()<0.001||vec2.Pt()<0.001) return -2;
+  
+  TLorentzVector sumVec = tHqUtils::GetTLorentzVector(vec1+vec2);
+  TVector3 cmboost = -sumVec.BoostVector();
+  
+  TLorentzVector boostedvec1 = GetTLorentzVector(vec1);
+  boostedvec1.Boost(cmboost);
+  
+  return cos( sumVec.Angle(boostedvec1.Vect()) );
+}
+
+
+float tHqUtils::CosThetaCM(const math::XYZTLorentzVector& vec,const math::XYZTLorentzVector& boostVec){
+  if(vec.Pt()<0.001||boostVec.Pt()<0.001) return -2;
+  
+  TLorentzVector vec_ = GetTLorentzVector(vec);
+  TLorentzVector boostVec_ = GetTLorentzVector(boostVec);
+  
+  TVector3 zBoost = TVector3(0,0,-boostVec_.BoostVector().Pz());
+  TLorentzVector boostedVec = vec_;
+  boostedVec.Boost(zBoost);
+  float theta = boostedVec.Theta();
+  return TMath::Cos(theta);
+}
+
+
+std::vector<math::XYZTLorentzVector> tHqUtils::GetGenParticleVecs(const std::vector<reco::GenParticle>& genParticles){
+  std::vector<math::XYZTLorentzVector> genParticleVecs;
+  
+  for(std::vector<reco::GenParticle>::const_iterator itPart=genParticles.begin();itPart!=genParticles.end();++itPart){
+    genParticleVecs.push_back(itPart->p4());
+  }
+  
+  return genParticleVecs;
+}
+
+
+bool tHqUtils::MCContainsTTbar(const std::vector<reco::GenParticle>& genParticles){
+  bool foundT=false;
+  bool foundTbar=false;
+  for(size_t i=0; i<genParticles.size();i++){
+    if(genParticles[i].pdgId()==6) foundT=true;
+    if(genParticles[i].pdgId()==-6) foundTbar=true;
+  }
+  return foundT&&foundTbar;
+}
+
+bool tHqUtils::MCContainsST(const std::vector<reco::GenParticle>& genParticles){
+  bool foundT=false;
+  bool foundTbar=false;
+  for(size_t i=0; i<genParticles.size();i++){
+    if(genParticles[i].pdgId()==6) foundT=true;
+    if(genParticles[i].pdgId()==-6) foundTbar=true;
+  }
+  return (foundT&&!foundTbar)||(!foundT&&foundTbar);
+}
+
+
+
+bool tHqUtils::MCContainsHiggs(const std::vector<reco::GenParticle>& genParticles){
+  for(size_t i=0; i<genParticles.size();i++){
+    if(genParticles[i].pdgId()==25) return true;
+  }
+  return false;
+}
+
+
+void tHqUtils::GetttHMCParticles(const std::vector<reco::GenParticle>& genParticles, std::vector<reco::GenParticle>& tophad, std::vector<reco::GenParticle>& whad, std::vector<reco::GenParticle>& bhad, std::vector<reco::GenParticle>& q1, std::vector<reco::GenParticle>& q2, std::vector<reco::GenParticle>& toplep, std::vector<reco::GenParticle>& wlep, std::vector<reco::GenParticle>& blep, std::vector<reco::GenParticle>& lep, std::vector<reco::GenParticle>& nu, reco::GenParticle& higgs, reco::GenParticle& b1, reco::GenParticle& b2){
+  std::vector<reco::GenParticle> leptonsFromWplus;
+  std::vector<reco::GenParticle> leptonsFromWminus;
+  std::vector<reco::GenParticle> quarksFromWplus;
+  std::vector<reco::GenParticle> quarksFromWminus;
+  std::vector<reco::GenParticle> quarksFromT;
+  std::vector<reco::GenParticle> quarksFromTbar;
+  std::vector<reco::GenParticle> bquarksFromHiggs;
+  reco::GenParticle Wplus;
+  reco::GenParticle Wminus;
+  reco::GenParticle top;
+  reco::GenParticle topbar;
+  
+  for(std::vector<reco::GenParticle>::const_iterator itPart = genParticles.begin();itPart != genParticles.end();itPart++) {
+    if(itPart->numberOfMothers()==0) continue;
+    if(itPart->mother()->pdgId()==6    && abs(itPart->pdgId())<6) quarksFromT.push_back(*itPart);
+    if(itPart->mother()->pdgId()==-6   && abs(itPart->pdgId())<6) quarksFromTbar.push_back(*itPart);
+    if(itPart->mother()->pdgId()==24   && abs(itPart->pdgId())<6) quarksFromWplus.push_back(*itPart);
+    if(itPart->mother()->pdgId()==-24  && abs(itPart->pdgId())<6) quarksFromWminus.push_back(*itPart);
+    if(itPart->mother()->pdgId()==24   && abs(itPart->pdgId())>=11&&abs(itPart->pdgId())<=16) leptonsFromWplus.push_back(*itPart);
+    if(itPart->mother()->pdgId()==-24  && abs(itPart->pdgId())>=11&&abs(itPart->pdgId())<=16) leptonsFromWminus.push_back(*itPart);
+    if(itPart->mother()->pdgId()==25   && abs(itPart->pdgId())==5) bquarksFromHiggs.push_back(*itPart);
+    
+    if(itPart->mother()->pdgId()==6    && itPart->pdgId()==24)    Wplus = *itPart;
+    if(itPart->mother()->pdgId()==-6   && itPart->pdgId()==-24)   Wminus = *itPart;
+    
+    if(itPart->pdgId()==25 || fabs(itPart->pdgId())==6){
+      bool lastHiggs = true;
+      bool lastTop = true;
+      bool lastTopBar = true;
+
+      for(size_t iDaughter = 0;iDaughter<itPart->numberOfDaughters();++iDaughter){
+        if(itPart->daughter(iDaughter)->pdgId()==25) lastHiggs = false;
+        if(itPart->daughter(iDaughter)->pdgId()==6) lastTop = false;
+        if(itPart->daughter(iDaughter)->pdgId()==-6) lastTopBar = false;
+      }
+
+      if(lastHiggs  && itPart->pdgId()==25) higgs = *itPart;
+      if(lastTop    && itPart->pdgId()==6)  top = *itPart;
+      if(lastTopBar && itPart->pdgId()==-6) topbar = *itPart; 
+    }
+  }
+  
+  if(leptonsFromWplus.size()==2 && quarksFromT.size()>=1){
+    toplep.push_back(top);
+    wlep.push_back(Wplus);
+    blep.push_back(quarksFromT[0]);
+    if(leptonsFromWplus[0].pdgId()%2!=0){
+      lep.push_back(leptonsFromWplus[0]);
+      nu.push_back(leptonsFromWplus[1]);
+    }
+    else{
+      lep.push_back(leptonsFromWplus[1]);
+      nu.push_back(leptonsFromWplus[0]);
+    }
+  }
+  if(leptonsFromWminus.size()==2 && quarksFromTbar.size()>=1){
+    toplep.push_back(topbar);
+    wlep.push_back(Wminus);
+    blep.push_back(quarksFromTbar[0]);
+    if(leptonsFromWminus[0].pdgId()%2!=0){
+      lep.push_back(leptonsFromWminus[0]);
+      nu.push_back(leptonsFromWminus[1]);
+    }
+    else{
+      lep.push_back(leptonsFromWminus[1]);
+      nu.push_back(leptonsFromWminus[0]);
+    }
+  }
+  if(quarksFromWplus.size()==2 && quarksFromT.size()>=1){
+    tophad.push_back(top);
+    whad.push_back(Wplus);
+    bhad.push_back(quarksFromT[0]);
+    q1.push_back(quarksFromWplus[0]);
+    q2.push_back(quarksFromWplus[1]);
+  }
+  if(quarksFromWminus.size()==2 && quarksFromTbar.size()>=1){
+    tophad.push_back(topbar);
+    whad.push_back(Wminus);
+    bhad.push_back(quarksFromTbar[0]);
+    q1.push_back(quarksFromWminus[0]);
+    q2.push_back(quarksFromWminus[1]);
+  }
+  if(bquarksFromHiggs.size()==2){
+    b1 = bquarksFromHiggs[0];
+    b2 = bquarksFromHiggs[1];
+  }  
+}
+
+
+void tHqUtils::GetttHMCVecs(const std::vector<reco::GenParticle>& genParticles, std::vector<math::XYZTLorentzVector>& tophadvecs, std::vector<math::XYZTLorentzVector>& whadvecs, std::vector<math::XYZTLorentzVector>& bhadvecs, std::vector<math::XYZTLorentzVector>& q1vecs, std::vector<math::XYZTLorentzVector>& q2vecs, std::vector<math::XYZTLorentzVector>& toplepvecs, std::vector<math::XYZTLorentzVector>& wlepvecs, std::vector<math::XYZTLorentzVector>& blepvecs, std::vector<math::XYZTLorentzVector>& lepvecs, std::vector<math::XYZTLorentzVector>& nuvecs, math::XYZTLorentzVector& higgsvec, math::XYZTLorentzVector& b1vec, math::XYZTLorentzVector& b2vec){
+  
+  std::vector<reco::GenParticle> tophad;
+  std::vector<reco::GenParticle> whad;
+  std::vector<reco::GenParticle> bhad;
+  std::vector<reco::GenParticle> q1;
+  std::vector<reco::GenParticle> q2;
+  std::vector<reco::GenParticle> toplep;
+  std::vector<reco::GenParticle> wlep;
+  std::vector<reco::GenParticle> blep;
+  std::vector<reco::GenParticle> lep;
+  std::vector<reco::GenParticle> nu;
+  reco::GenParticle higgs;
+  reco::GenParticle b1;
+  reco::GenParticle b2;
+  
+  GetttHMCParticles(genParticles,tophad,whad,bhad,q1,q2,toplep,wlep,blep,lep,nu,higgs,b1,b2);
+  
+  tophadvecs = GetGenParticleVecs(tophad);
+  whadvecs = GetGenParticleVecs(whad);
+  bhadvecs = GetGenParticleVecs(bhad);
+  q1vecs = GetGenParticleVecs(q1);
+  q2vecs = GetGenParticleVecs(q2);
+  toplepvecs = GetGenParticleVecs(toplep);
+  wlepvecs = GetGenParticleVecs(wlep);
+  blepvecs = GetGenParticleVecs(blep);
+  lepvecs = GetGenParticleVecs(lep);
+  nuvecs = GetGenParticleVecs(nu);
+  higgsvec = higgs.p4();
+  b1vec = b1.p4();
+  b2vec = b2.p4();
+}
+
+
+bool tHqUtils::IsAnyTriggerBitFired(const std::vector<string> & targetTriggers, const edm::TriggerResults& triggerResults){
+  
+  // check to see if you passed the trigger by looping over the bits
+  // looking for your bit
+  // and see if it is 1
+  
+  for(vector<string>::const_iterator iTarget = targetTriggers.begin();iTarget != targetTriggers.end();iTarget++) {
+    
+    if(*iTarget == "None") return true;
+    
+    // if this is the right name and the bit is set to one
+    int TriggerID = triggerResults.find(*iTarget);
+    
+    if(triggerResults.accept(TriggerID)==1) return true;
+    
+  }// end for each target
+  
+  return false;
+  
+}
+
+
+std::vector<math::XYZTLorentzVector> tHqUtils::GetLepVecs(const std::vector<pat::Electron>& selectedElectrons, const std::vector<pat::Muon> selectedMuons){
+  
+  std::vector<math::XYZTLorentzVector> leptonVecs;
+  
+  for(std::vector<pat::Electron>::const_iterator itEle = selectedElectrons.begin(); itEle != selectedElectrons.end(); ++itEle){
+    leptonVecs.push_back(itEle->p4());
+  }
+  for(std::vector<pat::Muon>::const_iterator itMu = selectedMuons.begin(); itMu != selectedMuons.end(); ++itMu){
+    leptonVecs.push_back(itMu->p4());
+  }
+  
+  std::sort(leptonVecs.begin(), leptonVecs.end(),tHqUtils::FirstIsHarder);
+  
+  return leptonVecs;
+  
+} 
+
+
+math::XYZTLorentzVector tHqUtils::GetPrimLepVec(const std::vector<pat::Electron>& selectedElectrons,const std::vector<pat::Muon> selectedMuons){
+  
+  std::vector<math::XYZTLorentzVector> leptonVecs = GetLepVecs(selectedElectrons,selectedMuons);
+  
+  if(leptonVecs.size()==0){
+    std::cerr<< "No PrimLep Found!" << std::endl;
+    return math::XYZTLorentzVector();
+  }
+  
+  return leptonVecs[0];
+}
+
+
+void tHqUtils::GetNuVecs(const math::XYZTLorentzVector& lepvec, const TVector2& metvec, math::XYZTLorentzVector& nu1, math::XYZTLorentzVector& nu2){
+  
+  double mu = (80.4*80.4)/2 + metvec.Px()*lepvec.Px() + metvec.Py()*lepvec.Py();
+  double a = (mu*lepvec.Pz())/(lepvec.E()*lepvec.E() - lepvec.Pz()*lepvec.Pz());
+  double a2 = TMath::Power(a, 2);
+  double b = (TMath::Power(lepvec.E(), 2.)*metvec.Mod() - TMath::Power(mu, 2.)) / (TMath::Power(lepvec.E(), 2)- TMath::Power(lepvec.Pz(), 2));
+  float pz1,pz2;
+  
+  if (a2-b < 0) { 
+    pz1 = a;
+    pz2 = a;
+  } else {
+    double root = sqrt(a2-b);
+    pz1 = a + root;
+    pz2 = a - root;
+  }
+  
+  nu1.SetPxPyPzE(metvec.Px(),metvec.Py(),pz1,sqrt(metvec.Mod2()+pz1*pz1));
+  nu2.SetPxPyPzE(metvec.Px(),metvec.Py(),pz2,sqrt(metvec.Mod2()+pz2*pz2));
+}
+
+
+vector<math::XYZTLorentzVector> tHqUtils::GetJetVecs(const std::vector<pat::Jet>& jets){
+  std::vector<math::XYZTLorentzVector> jetVecs;
+  
+  for(std::vector<pat::Jet>::const_iterator itJet=jets.begin();itJet!=jets.end();++itJet){
+    jetVecs.push_back(itJet->p4());
+  }
+  
+  return jetVecs;
+}
+
+
+bool tHqUtils::PassesCSV(const pat::Jet& jet, const char workingPoint){
+  
+  float CSVLv2wp = 0.423;
+  float CSVMv2wp = 0.814;
+  float CSVTv2wp = 0.941;
+  
+  float csvValue = jet.bDiscriminator("combinedInclusiveSecondaryVertexV2BJetTags");
+  
+  switch(workingPoint){
+    case 'L': if(csvValue > CSVLv2wp){ return true; } break;
+    case 'M': if(csvValue > CSVMv2wp){ return true; } break;
+    case 'T': if(csvValue > CSVTv2wp){ return true; } break;
+    case '-': return true; break;
+  }
+  
+  return false;
+}
+
+
+float tHqUtils::GetClosestJetIDs(int& idJet1, int& idJet2, const std::vector<pat::Jet>& jets){
+  
+  float minDr = 9.;
+  for(std::vector<pat::Jet>::const_iterator itJet1 = jets.begin();itJet1 != jets.end();++itJet1){
+    for(std::vector<pat::Jet>::const_iterator itJet2 = itJet1+1;itJet2 != jets.end();++itJet2){
+      math::XYZTLorentzVector jetVec1 = itJet1->p4();
+      math::XYZTLorentzVector jetVec2 = itJet2->p4();
+      
+      if(tHqUtils::DeltaR(jetVec1,jetVec2)<minDr){
+	      idJet1 = itJet1 - jets.begin();
+	      idJet2 = itJet2 - jets.begin();
+	      minDr = tHqUtils::DeltaR(jetVec1,jetVec2);
+      }
+    }
+  }
+  
+  return minDr;
+}
+
+
+float tHqUtils::GetClosestLepJetID(int& idJet, const math::XYZTLorentzVector& lepVec, const std::vector<pat::Jet>& jets){
+  
+  float minDr = 9.;
+  for(std::vector<pat::Jet>::const_iterator itJet = jets.begin(); itJet != jets.end(); ++itJet){
+    math::XYZTLorentzVector jetVec = itJet->p4();
+    if(DeltaR(lepVec,jetVec)<minDr){
+	    idJet = itJet - jets.begin();
+	    minDr = DeltaR(lepVec,jetVec);
+    }
+  }
+
+  return minDr;
+}
+
+
+float tHqUtils::GetJetAverageJetEtaMax(const std::vector<pat::Jet>& jets1, const std::vector<pat::Jet>& jets2){
+  int count=0;
+  float avgval=0.;
+  for(std::vector<pat::Jet>::const_iterator itJet1 = jets1.begin(); itJet1 != jets1.end(); ++itJet1){
+    for(std::vector<pat::Jet>::const_iterator itJet2 = itJet1+1; itJet2 != jets1.end(); ++itJet2){
+      avgval += itJet1->eta()-itJet2->eta();
+      count++;
+    }
+  }
+  avgval /= count;
+  
+  float imax = 0.;
+  float etamax=0.;
+  for(std::vector<pat::Jet>::const_iterator itJet = jets2.begin(); itJet != jets2.end(); ++itJet){
+    imax = abs(itJet->eta()-avgval);
+    if(imax>etamax) {
+      etamax = imax;
+    }
+  }
+  return etamax;
+}
+
+
+void tHqUtils::GetFoxWolframMoments(std::vector<math::XYZTLorentzVector> jetVecs, float &h0, float &h1, float &h2, float &h3, float &h4) {
+  //
+  // Aplanarity and sphericity
+  //
+  
+  float eVis = 0.0;
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetVecs.begin();itJetVec != jetVecs.end();++itJetVec){
+    eVis += itJetVec->energy();
+  }
+  h0 = 0.0;
+  h1 = 0.0;
+  h2 = 0.0;
+  h3 = 0.0;
+  h4 = 0.0;
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec1 = jetVecs.begin();itJetVec1 != jetVecs.end();++itJetVec1){
+    for(std::vector<math::XYZTLorentzVector>::iterator itJetVec2 = itJetVec1+1;itJetVec2 != jetVecs.end();++itJetVec2){
+      float costh = ROOT::Math::VectorUtil::CosTheta(*itJetVec1,*itJetVec2);
+      float p0 = 1.0;
+      float p1 = costh;
+      float p2 = 0.5*(3.0*costh*costh - 1.0);
+      float p3 = 0.5*(5.0*costh*costh - 3.0*costh);
+      float p4 = 0.125*(35.0*costh*costh*costh*costh - 30.0*costh*costh + 3.0);
+      float pipj = itJetVec1->P()*itJetVec2->P();
+      h0 += (pipj/(eVis*eVis))*p0;
+      h1 += (pipj/(eVis*eVis))*p1;
+      h2 += (pipj/(eVis*eVis))*p2;
+      h3 += (pipj/(eVis*eVis))*p3;
+      h4 += (pipj/(eVis*eVis))*p4;
+    }
+  }
+  return;
+}
+
+
+void tHqUtils::GetAplanaritySphericity(math::XYZTLorentzVector leptonVec, math::XYZTLorentzVector metVec, std::vector<math::XYZTLorentzVector> jetVecs, float &aplanarity, float &sphericity){
+  //
+  // Aplanarity and sphericity
+  //
+  float mxx = leptonVec.Px()*leptonVec.Px() + metVec.Px()*metVec.Px();
+  float myy = leptonVec.Py()*leptonVec.Py() + metVec.Py()*metVec.Py();
+  float mzz = leptonVec.Pz()*leptonVec.Pz() + metVec.Pz()*metVec.Pz();
+  float mxy = leptonVec.Px()*leptonVec.Py() + metVec.Px()*metVec.Py();
+  float mxz = leptonVec.Px()*leptonVec.Pz() + metVec.Px()*metVec.Pz();
+  float myz = leptonVec.Py()*leptonVec.Pz() + metVec.Px()*metVec.Pz();
+  
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec=jetVecs.begin();itJetVec!=jetVecs.end();++itJetVec){
+    mxx += itJetVec->Px()*itJetVec->Px();
+    myy += itJetVec->Py()*itJetVec->Py();
+    mzz += itJetVec->Pz()*itJetVec->Pz();
+    mxy += itJetVec->Px()*itJetVec->Py();
+    mxz += itJetVec->Px()*itJetVec->Pz();
+    myz += itJetVec->Py()*itJetVec->Pz();
+  }
+  float sum = mxx + myy + mzz;
+  mxx /= sum;
+  myy /= sum;
+  mzz /= sum;
+  mxy /= sum;
+  mxz /= sum;
+  myz /= sum;
+  
+  TMatrix tensor(3,3);
+  tensor(0,0) = mxx;
+  tensor(1,1) = myy;
+  tensor(2,2) = mzz;
+  tensor(0,1) = mxy;
+  tensor(1,0) = mxy;
+  tensor(0,2) = mxz;
+  tensor(2,0) = mxz;
+  tensor(1,2) = myz;
+  tensor(2,1) = myz;
+  TVector eigenval(3);
+  tensor.EigenVectors(eigenval);
+  
+  sphericity = 3.0*(eigenval(1)+eigenval(2))/2.0;
+  aplanarity = 3.0*eigenval(2)/2.0;
+  
+  return;
+}
+
+
