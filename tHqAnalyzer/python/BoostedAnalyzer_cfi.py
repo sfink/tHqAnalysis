59c098180d31c2c241d3c79fa44edfc3b51502b1
Simon Fink <simon.fink@kit.edu>
2015-04-17 10:25:08 +0200

    added python files and test script

---
+++ tHqAnalyzer/python/BoostedAnalyzer_cfi.py
@@ -0,0 +1,21 @@
+import FWCore.ParameterSet.Config as cms
+from tHqAnalysis.tHqAnalyzer.Selection_cff import *
+
+tHqAnalyzer = cms.EDAnalyzer(
+    'tHqAnalyzer',
+    LeptonSelectionNoTrigger,
+    JetTagSelection,  
+    era = cms.string("2012_53x"),
+    analysisType = cms.string("LJ"),
+    luminostiy = cms.double(19.7),
+    sampleID = cms.int32(9125),
+    xs = cms.double(248),
+    nMCEvents = cms.int32(25000000),
+    isData = cms.bool(False),
+    useFatJets = cms.bool(False),
+    disableObjectSelections = cms.bool(False), # disables selection of some objects for synch exe
+    outfileName = cms.string("tHqAnalyzed"),
+    selectionNames = cms.vstring("LeptonSelection"),
+    processorNames = cms.vstring()
+#    processorNames = cms.vstring("WeightProcessor","MCMatchVarProcessor","MVAVarProcessor","tHqJetVarProcessor","tHqTopHiggsVarProcessor","tHqTopVarProcessor","tHqHiggsVarProcessor")
+)
