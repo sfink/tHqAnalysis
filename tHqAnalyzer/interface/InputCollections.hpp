4dbbefc6c1da20f0d8e98eb40d5f026d9397d0ec
Simon Fink <simon.fink@kit.edu>
2015-08-18 16:56:39 +0200

    first changes to plugin, does not compile yet

--- tHqAnalyzer/interface/InputCollections.hpp
+++ tHqAnalyzer/interface/InputCollections.hpp
@@ -13,76 +13,75 @@
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/MET.h"
 #include "HLTrigger/HLTcore/interface/HLTConfigProvider.h"
+//#include "tHqAnalysis/tHqAnalyzer/interface/GenTopEvent.hpp"
+#include "tHqAnalysis/tHqAnalyzer/interface/EventInfo.hpp"
+//#include "tHqAnalysis/tHqAnalyzer/interface/BoostedUtils.hpp"
+#include "MiniAOD/MiniAODHelper/interface/MiniAODHelper.h"
 
 
 enum SampleType{data,tth,tt,nonttbkg};
 
 struct InputCollections{
-  InputCollections( const boosted::Event&                         event_,
+  InputCollections( const EventInfo&                              eventInfo_,
 		    const TriggerInfo&                            triggerInfo_,
-		    //                    const pat::TriggerObjectStandAloneCollection& selectedTrigger_,
-		    //                    const edm::TriggerResults&                    triggerResults_,
-		    //                    const HLTConfigProvider&                      hlt_config_,
-                    const reco::VertexCollection&                 selectedPVs_,
+                    const std::vector<reco::Vertex>&              selectedPVs_,
+		    const std::vector<pat::Muon>&                 rawMuons_,
                     const std::vector<pat::Muon>&                 selectedMuons_,
                     const std::vector<pat::Muon>&                 selectedMuonsLoose_,
+                    const std::vector<pat::Electron>&             rawElectrons_,
                     const std::vector<pat::Electron>&             selectedElectrons_,
                     const std::vector<pat::Electron>&             selectedElectronsLoose_,
+                    const std::vector<pat::Jet>&                  rawJets_,
                     const std::vector<pat::Jet>&                  selectedJets_,
+                    const std::vector<pat::Jet>&                  rawPuppiJets_,
                     const std::vector<pat::Jet>&                  selectedJetsLoose_,
                     const std::vector<pat::Jet>&                  selectedPuppiJets_,
-                    const std::vector<pat::MET>&                  pfMets_,
-                    const std::vector<reco::GenParticle>&         genParticles_,
+                    const pat::MET&                               pfMET_,
+		    //		    const GenTopEvent&                            genTopEvt_,
                     const std::vector<reco::GenJet>&              selectedGenJets_,
                     const SampleType                              sampleType_,
                     const std::map<std::string,float>&            weights_,
-		    const edm::EventSetup&	                  setup_,
-        	    const edm::Event&	                          edmevent_
-
                   ):
-                    event(event_),
+                    eventInfo(eventInfo_),
 		    triggerInfo(triggerInfo_),
-		    //		    selectedTrigger(selectedTrigger_),
-		    //                    triggerResults(triggerResults_),
-		    //                    hlt_config(hlt_config_),
                     selectedPVs(selectedPVs_),
+		    rawMuons(rawMuons_),
                     selectedMuons(selectedMuons_),
                     selectedMuonsLoose(selectedMuonsLoose_),
+		    rawElectrons(rawElectrons_),
                     selectedElectrons(selectedElectrons_),
                     selectedElectronsLoose(selectedElectronsLoose_),
+		    rawJets(rawJets_),
                     selectedJets(selectedJets_),
+                    rawPuppiJets(rawPuppiJets_),
                     selectedJetsLoose(selectedJetsLoose_),
                     selectedPuppiJets(selectedPuppiJets_),
-                    pfMets(pfMets_),
-                    genParticles(genParticles_),
+                    pfMET(pfMET_),
+		    //                    genTopEvt(genTopEvt_),
                     selectedGenJets(selectedGenJets_),
                     sampleType(sampleType_),
-                    weights(weights_),
-		    setup(setup_),
-                    edmevent(edmevent_){}
+                    weights(weights_)
+                    {}
   
-  const boosted::Event&                         event;
+  const EventInfo&                              eventInfo;
   const TriggerInfo&                            triggerInfo;
-  //  const pat::TriggerObjectStandAloneCollection& selectedTrigger;
-  //  const edm::TriggerResults&                    triggerResults;
-  //  const HLTConfigProvider&                       hlt_config;
-  const reco::VertexCollection&                 selectedPVs;
+  const std::vector<reco::Vertex>&              selectedPVs;
+  const std::vector<pat::Muon>&                 rawMuons;
   const std::vector<pat::Muon>&                 selectedMuons;
   const std::vector<pat::Muon>&                 selectedMuonsLoose;
+  const std::vector<pat::Electron>&             rawElectrons;
   const std::vector<pat::Electron>&             selectedElectrons;
   const std::vector<pat::Electron>&             selectedElectronsLoose;
+  const std::vector<pat::Jet>&                  rawJets;
   const std::vector<pat::Jet>&                  selectedJets;
+  const std::vector<pat::Jet>&                  rawPuppiJets;
   const std::vector<pat::Jet>&                  selectedJetsLoose;
   const std::vector<pat::Jet>&                  selectedPuppiJets;
-  const std::vector<pat::MET>&                  pfMets;
-  const std::vector<reco::GenParticle>&         genParticles;
+  const pat::MET&                               pfMET;
+  //  const GenTopEvent&                            genTopEvt;
   const std::vector<reco::GenJet>&              selectedGenJets;
   const SampleType                              sampleType;
   const std::map<std::string,float>&            weights;
-  const edm::EventSetup& 			setup;
-  const edm::Event& 			        edmevent;
-
-  void Dump();
 };
 
 #endif
