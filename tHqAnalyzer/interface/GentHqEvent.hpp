7503b84eb9abe787390a515c644d828cfb71e88c
Benedikt Maier <benedikt.maier@cern.ch>
2015-11-02 14:22:24 +0100

    tHq MC container

--- tHqAnalyzer/interface/GentHqEvent.hpp
+++ tHqAnalyzer/interface/GentHqEvent.hpp
@@ -9,131 +9,23 @@ class GentHqEvent{
 public:
   GentHqEvent();
   ~GentHqEvent();
-  void Fill(const std::vector<reco::GenParticle>& prunedGenParticles, int ttXid);
-  void FillTTxDetails(const std::vector<reco::GenJet>& customGenJets, 
-		      const std::vector<int>& genBHadIndex, const std::vector<int>& genBHadJetIndex, 
-		      const std::vector<int>& genBHadFlavour, const std::vector<int>& genBHadFromTopWeakDecay, 
-		      const std::vector<reco::GenParticle>& genBHadPlusMothers, 
-		      const std::vector<int>& genCHadIndex, const std::vector<int>& genCHadJetIndex, 
-		      const std::vector<int>& genCHadFlavour, const std::vector<int>& genCHadFromTopWeakDecay, 
-		      const std::vector<reco::GenParticle>& genCHadPlusMothers,
-		      const std::vector<int>& genCHadBHadronId,
-		      const float ttxptcut,const float  ttxetacut);  
+  void Fill(const std::vector<reco::GenParticle>& prunedGenParticles);
   
-  reco::GenParticle GetHiggs() const;
+
   std::vector<reco::GenParticle> GetHiggsDecayProducts() const;
   bool IsFilled() const;
-  bool TTxIsFilled() const;
   reco::GenParticle GetTop() const;
-  reco::GenParticle GetTopBar() const;
+  reco::GenParticle GetHiggs() const;
   reco::GenParticle GetTopDecayQuark() const;
-  reco::GenParticle GetTopBarDecayQuark() const;
-
-  reco::GenParticle GetWplus() const;
-  reco::GenParticle GetWminus() const;
-  std::vector<reco::GenParticle> GetWplusDecayProducts() const;
-  std::vector<reco::GenParticle> GetWminusDecayProducts() const;
-
-  reco::GenParticle GetTopHad() const;
-  reco::GenParticle GetTopLep() const;
-  reco::GenParticle GetTopHadDecayQuark() const;
-  reco::GenParticle GetTopLepDecayQuark() const;
-  reco::GenParticle GetWhad() const;
-  reco::GenParticle GetWlep() const;
-  std::vector<reco::GenParticle> GetWLeptons() const;
-  reco::GenParticle GetLepton() const;
-  reco::GenParticle GetNeutrino() const;
-  std::vector<reco::GenParticle> GetWQuarks() const;
-
-  std::vector<reco::GenParticle> GetAllTopHads() const;
-  std::vector<reco::GenParticle> GetAllTopLeps() const;
-  std::vector<reco::GenParticle> GetAllTopHadDecayQuarks() const;
-  std::vector<reco::GenParticle> GetAllTopLepDecayQuarks() const;
-  std::vector<reco::GenParticle> GetAllWhads() const;
-  std::vector<reco::GenParticle> GetAllWleps() const;
-  std::vector<reco::GenParticle> GetAllLeptons() const;
-  std::vector<reco::GenParticle> GetAllNeutrinos() const;
-  std::vector<reco::GenParticle> GetAllWQuarks() const;
-  std::vector<reco::GenParticle> GetAllWAntiQuarks() const;
-
-  std::vector<reco::GenParticle> GetQuarks() const;
-
-  math::XYZTLorentzVector GetHiggsVec() const;
-  math::XYZTLorentzVector GetTopVec() const;
-  math::XYZTLorentzVector GetTopBarVec() const;
-  math::XYZTLorentzVector GetTopHadVec() const;
-  math::XYZTLorentzVector GetTopLepVec() const;
-  math::XYZTLorentzVector GetWplusVec() const;
-  math::XYZTLorentzVector GetWminusVec() const;
-  math::XYZTLorentzVector GetWhadVec() const;
-  math::XYZTLorentzVector GetWlepVec() const;
-  math::XYZTLorentzVector GetLeptonVec() const;
-  math::XYZTLorentzVector GetNeutrinoVec() const;
-  std::vector<math::XYZTLorentzVector> GetQuarkVecs() const;
-  std::vector<math::XYZTLorentzVector> GetHiggsDecayProductVecs() const;
-  math::XYZTLorentzVector GetTopDecayQuarkVec() const;
-  math::XYZTLorentzVector GetTopBarDecayQuarkVec() const;
-  math::XYZTLorentzVector GetTopHadDecayQuarkVec() const;
-  math::XYZTLorentzVector GetTopLepDecayQuarkVec() const;
-
-  std::vector<math::XYZTLorentzVector> GetWLeptonsVecs() const;
-  std::vector<math::XYZTLorentzVector> GetWQuarksVecs() const;
-  std::vector<math::XYZTLorentzVector> GetWplusDecayProductsVecs() const;
-  std::vector<math::XYZTLorentzVector> GetWminusDecayProductsVecs() const;
-
-  std::vector<math::XYZTLorentzVector> GetAllTopHadVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllTopLepVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllTopHadDecayQuarkVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllTopLepDecayQuarkVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllWhadVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllWlepVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllLeptonVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllNeutrinoVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllWQuarkVecs() const;
-  std::vector<math::XYZTLorentzVector> GetAllWAntiQuarkVecs() const;
-
-  std::vector<reco::GenJet> GetAllTopHadBGenJets() const;
-  std::vector<reco::GenJet> GetAllTopLepBGenJets() const;
-  reco::GenJet GetTopBarBBarGenJet() const;
-  reco::GenJet GetTopBGenJet() const;
-  reco::GenJet GetHiggsBGenJet() const;
-  reco::GenJet GetHiggsBBarGenJet() const;
-
-  std::vector<reco::GenJet> GetAdditionalBGenJets() const;
-  std::vector<reco::GenJet> GetAdditionalCGenJets() const;
-  std::vector<reco::GenParticle> GetAdditionalBGenJetsHadron() const;
-  std::vector<reco::GenParticle> GetAdditionalCGenJetsHadron() const;
-
-
-  std::vector<int> GetAdditionalCGenJetsFromTopType() const;
-  std::vector<int> GetAdditionalBGenJetsFromTopType() const;
-  std::vector<int> GetAdditionalCGenJetsNHadrons() const;
-  std::vector<int> GetAdditionalBGenJetsNHadrons() const;
-  std::vector<reco::GenParticle> GetAllTopHadBHadrons() const;
-  std::vector<reco::GenParticle> GetAllTopLepBHadrons() const;
-  reco::GenParticle GetTopBarBBarHadron() const;
-  reco::GenParticle GetTopBHadron() const;
-  reco::GenParticle GetHiggsBHadron() const;
-  reco::GenParticle GetHiggsBBarHadron() const;
-  reco::GenParticle GetWCHadron() const;
-
-  std::vector<reco::GenParticle> GetAdditionalBHadrons() const;
-  std::vector<int> GetAdditionalBHadronMothers() const;
-  std::vector<int> GetAdditionalBHadronAfterTopType() const;
-  std::vector<reco::GenParticle> GetAdditionalCHadrons() const;
-  std::vector<int> GetAdditionalCHadronMothers() const;
-  std::vector<int> GetAdditionalCHadronAfterTopType() const;
-
-  int GetTTxId(bool countAdditionalAfterTop=true) const;
-  int GetTTxIdFromHelper() const;
-
-
-  bool IsAllHadron() const;
-  bool IsDiLepton() const;
-  bool IsSemiLepton() const;
+  reco::GenParticle GetW() const;
   
+  std::vector<reco::GenParticle> GetWDecayProducts() const;
+  std::vector<reco::GenParticle> GetHiggssDecayProducts() const;
+
+  reco::GenParticle GetSecondb() const;
+  reco::GenParticle GetLightQuark() const;
+
   void Print() const;
-  void PrintTTX() const;
 private:
   math::XYZTLorentzVector GetLV(const reco::GenParticle& p) const;
   std::vector<math::XYZTLorentzVector> GetLVs(const std::vector<reco::GenParticle>& ps) const;
@@ -142,52 +34,15 @@ private:
 
   reco::GenParticle higgs;
   reco::GenParticle top;
-  reco::GenParticle topbar;
-  reco::GenParticle wplus;
-  reco::GenParticle wminus;
+  reco::GenParticle wboson;
+  reco::GenParticle lightquark;
+  reco::GenParticle secondb;
   reco::GenParticle top_decay_quark;
-  reco::GenParticle topbar_decay_quark;
-  std::vector<reco::GenParticle> wplus_decay_products;
-  std::vector<reco::GenParticle> wminus_decay_products;
+  std::vector<reco::GenParticle> w_decay_products;
   std::vector<reco::GenParticle> higgs_decay_products;
 
-  reco::GenJet higgs_b_genjet;
-  reco::GenJet higgs_bbar_genjet;
-  reco::GenJet top_b_genjet;
-  reco::GenJet topbar_bbar_genjet;
-
-  std::vector<reco::GenJet> additional_b_genjets;
-  std::vector<int> additional_b_genjet_nb;
-  std::vector<int> additional_b_genjet_nb_aftertop;
-  std::vector<int> additional_b_genjet_aftertoptype;
-  std::vector<reco::GenParticle> additional_b_genjet_hadron;
-
-  reco::GenParticle higgs_b_hadron;
-  reco::GenParticle higgs_bbar_hadron;
-  reco::GenParticle top_b_hadron;
-  reco::GenParticle topbar_bbar_hadron;
-
-  std::vector<reco::GenParticle> additional_b_hadrons;
-  std::vector<int> additional_b_hadron_aftertop;
-  std::vector<int> additional_b_hadron_mother;
-
-  std::vector<reco::GenJet> additional_c_genjets;
-  std::vector<int> additional_c_genjet_nc;
-  std::vector<int> additional_c_genjet_nc_aftertop;
-  std::vector<int> additional_c_genjet_aftertoptype;
-  std::vector<reco::GenParticle> additional_c_genjet_hadron;
-
-  std::vector<reco::GenParticle> additional_c_hadrons;
-  std::vector<int> additional_c_hadron_aftertop;
-  std::vector<int> additional_c_hadron_mother;
-
-  reco::GenParticle w_c_hadron;
-
-  bool topIsHadronic;
-  bool topbarIsHadronic;
   bool isFilled;
-  bool ttxIsFilled;
-  int ttXid;
+
 };
 
 #endif
--- tHqAnalyzer/src/tHqGenVarProcessor.cpp
+++ tHqAnalyzer/src/tHqGenVarProcessor.cpp
@@ -1,4 +1,5 @@
 #include "tHqAnalysis/tHqAnalyzer/interface/tHqGenVarProcessor.hpp"
+#include "tHqAnalysis/tHqAnalyzer/interface/GentHqEvent.hpp"
 
 using namespace std;
 
@@ -44,6 +45,7 @@ void tHqGenVarProcessor::Init(const InputCollections& input,VariableContainer& v
   vars.InitVars( "Hdaueta",-9.,  "nHdau");
   vars.InitVars( "Hdauphi",-9., "nHdau" );
   vars.InitVars( "Hdaum",-9.,  "nHdau");
+  vars.InitVars( "Hdauid",-9.,  "nHdau");
 
   vars.InitVar( "nWdau", -9); 
 
@@ -51,17 +53,13 @@ void tHqGenVarProcessor::Init(const InputCollections& input,VariableContainer& v
   vars.InitVars( "Wdaueta",-9.,  "nWdau");
   vars.InitVars( "Wdauphi",-9., "nWdau" );
   vars.InitVars( "Wdaum",-9.,  "nWdau");
+  vars.InitVars( "Wdauid",-9.,  "nWdau");
  
-  vars.InitVar( "Hb1pt",-9. );
-  vars.InitVar( "Hb1eta",-9. );
-  vars.InitVar( "Hb1phi",-9. );
-  vars.InitVar( "Hb1idx",-9. );
- 
-  vars.InitVar( "Hb2pt",-9. );
-  vars.InitVar( "Hb2eta",-9. );
-  vars.InitVar( "Hb2phi",-9. );
-  vars.InitVar( "Hb2idx",-9. );
- 
+  vars.InitVars( "hbbjtidx", -9, "nHdau"); 
+  vars.InitVars( "Wjtidx", -9, "nWdau"); 
+  vars.InitVar( "tjtidx", -9);
+  vars.InitVar( "lqjtidx", -9);
+  vars.InitVar( "sbjtidx", -9);
 
   initialized = true;
 }
@@ -71,233 +69,136 @@ void tHqGenVarProcessor::Process(const InputCollections& input,VariableContainer
   if(!initialized) cerr << "tree processor not initialized" << endl;
 
   
-  std::vector<reco::GenParticle> H;
-  std::vector<reco::GenParticle> W;
-  std::vector<reco::GenParticle> t;
-  std::vector<reco::GenParticle> btop;
-  std::vector<reco::GenParticle> sb;
-  std::vector<reco::GenParticle> lq;
+  reco::GenParticle H;
+  reco::GenParticle W;
+  reco::GenParticle t;
+  reco::GenParticle btop;
+  reco::GenParticle sb;
+  reco::GenParticle lq;
   std::vector<reco::GenParticle> Hdau;
   std::vector<reco::GenParticle> Wdau;
-  if(input.gentHqEvt.IsFilled()){
-    H=input.gentHqEvt.GetAllHiggs();
-    W=input.gentHqEvt.GetAllWs();
-    t=input.gentHqEvt.GetAllTops();
-    btop=input.gentHqEvt.GetAllBtops();
-    sb=input.gentHqEvt.GetAllSecondBs();
-    lq=input.gentHqEvt.GetAllLightQuarks();
-    Hdau=input.gentHqEvt.GetAllHdaus();
-    Wdau=input.gentHqEvt.GetAllWdaus();
+  if(input.GentHqEvent.IsFilled()){
+    H=input.GentHqEvent.GetHiggs();
+    W=input.GentHqEvent.GetW();
+    t=input.GentHqEvent.GetTop();
+    btop=input.GentHqEvent.GetTopDecayQuark();
+    sb=input.GentHqEvent.GetSecondb();
+    lq=input.GentHqEvent.GetLightQuark();
+    Hdau=input.GentHqEvent.GetHiggsDecayProducts();
+    Wdau=input.GentHqEvent.GetWDecayProducts();
   }
 
-  std::cout << "#H: " << H.size() << endl;
-  std::cout << "#W: " << W.size() << endl;
-  std::cout << "#t: " << t.size() << endl;
-  std::cout << "#btops: " << btop.size() << endl;
-  std::cout << "#2ndBs: " << sb.size() << endl;
-  std::cout << "#LightQuarks: " << lq.size() << endl;
   std::cout << "#Hdaus: " << Hdau.size() << endl;
   std::cout << "#Wdaus: " << Wdau.size() << endl;
 
-  reco::GenParticle b1;
-  reco::GenParticle b2;
-  reco::GenParticle decProd1;
-  reco::GenParticle decProd2;
-
-  if(Hdau.size()>2)std::cout<<"MORE THAN TWO HIGGS PRODUCTS"<<std::endl;
-  bool dfirst=true;
-  for(auto p =Hdau.begin(); p!=Hdau.end(); p++){
-    if(p->pdgId()==5) b1=*p;
-    if(p->pdgId()==-5) b2=*p;
-    if(dfirst){
-      decProd1=*p;
-      dfirst=false;
-    }
-    else{
-      decProd2=*p;
-    }
-  }
-  
-  if(decProd1.pt()>0.){
-    vars.FillVar("Hb1pt",decProd1.pt());
-    vars.FillVar("Hb2pt",decProd2.pt());
-    vars.FillVar("Hb1eta",decProd1.eta());
-    vars.FillVar("Hb2eta",decProd2.eta());
-    vars.FillVar("Hb1phi",decProd1.phi());
-    vars.FillVar("Hb2phi",decProd2.phi());
-    vars.FillVar("Hb1id",decProd1.pdgId());
-    vars.FillVar("Hb2id",decProd2.pdgId());
-    
-  }
   
   vector<math::XYZTLorentzVector> jetvecs = tHqUtils::GetJetVecs(input.selectedJets);
 
-  for(size_t i=0;i<t.size();i++){
-    vars.FillVars( "tpt",i,t[i].pt());
-    vars.FillVars( "teta",i,t[i].eta());
-    vars.FillVars( "tphi",i,t[i].phi());
-    vars.FillVars( "tm",i,t[i].M());
-    vars.FillVars( "Wpt",i,W[i].pt());
-    vars.FillVars( "Weta",i,W[i].eta());
-    vars.FillVars( "Wphi",i,W[i].phi());
-    vars.FillVars( "Wm",i,W[i].M());
-    vars.FillVars( "btoppt",i,btop[i].pt());
-    vars.FillVars( "btopeta",i,btop[i].eta());
-    vars.FillVars( "btopphi",i,btop[i].phi());
-    vars.FillVars( "btopm",i,btop[i].M());
+  vars.FillVar( "tpt",t.pt());
+  vars.FillVar( "teta",t.eta());
+  vars.FillVar( "tphi",t.phi());
+  vars.FillVar( "tm",t.mass());
+  vars.FillVar( "Wpt",W.pt());
+  vars.FillVar( "Weta",W.eta());
+  vars.FillVar( "Wphi",W.phi());
+  vars.FillVar( "Wm",W.mass());
+  vars.FillVar( "Hpt",H.pt());
+  vars.FillVar( "Heta",H.eta());
+  vars.FillVar( "Hphi",H.phi());
+  vars.FillVar( "Hm",H.mass());
+  vars.FillVar( "btoppt",btop.pt());
+  vars.FillVar( "btopeta",btop.eta());
+  vars.FillVar( "btopphi",btop.phi());
+  vars.FillVar( "btopm",btop.mass());
+  vars.FillVar( "nHdau",Hdau.size());
+  vars.FillVar( "nWdau",Wdau.size());
+  for(size_t i=0;i<2;i++){
     vars.FillVars( "Wdaupt",i,Wdau[i].pt());
     vars.FillVars( "Wdaueta",i,Wdau[i].eta());
     vars.FillVars( "Wdauphi",i,Wdau[i].phi());
-    vars.FillVars( "Wdaum",i,Wdau[i].M());
-    
-    int idxblep = -1;
-    double minDrTopLep = 999;
-    for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
-      if(tHqUtils::DeltaR(*itJetVec,btop[i].p4())<minDrTopLep){
-        idxblep = itJetVec-jetvecs.begin();
-        minDrTopLep = tHqUtils::DeltaR(*itJetVec,btop[i].p4());
-      }
+    vars.FillVars( "Wdaum",i,Wdau[i].mass());
+    vars.FillVars( "Wdauid",i,Wdau[i].pdgId());
+    vars.FillVars( "Hdaupt",i,Hdau[i].pt());
+    vars.FillVars( "Hdaueta",i,Hdau[i].eta());
+    vars.FillVars( "Hdauphi",i,Hdau[i].phi());
+    vars.FillVars( "Hdaum",i,Hdau[i].mass());
+    vars.FillVars( "Hdauid",i,Hdau[i].pdgId());
+  }
+
+  int idxblep = -1;
+  double minDrTopLep = 999;
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
+    if(tHqUtils::DeltaR(*itJetVec,btop.p4())<minDrTopLep){
+      idxblep = itJetVec-jetvecs.begin();
+      minDrTopLep = tHqUtils::DeltaR(*itJetVec,btop.p4());
     }
-    
-    if(minDrTopLep<.25){
-      vars.FillVars( "btop_idx",i,idxblep);
-    }
-    std::cout << "Found Jet to b from top: #" << idxblep << "  with dR= " << minDrTopLep << endl; 
   }
   
-  if(H.pt()>0.){
-    vars.FillVar( "Hpt",H.pt());
-    vars.FillVar( "Heta",H.eta());
-    vars.FillVar( "Hphi",H.phi());
-    vars.FillVar( "Hm",H.M());
-  }
-  if(b1.pt()>0.){
-    vars.FillVar("Hb1pt",b1.pt());
-    vars.FillVar("Hb2pt",b2.pt());
-    vars.FillVar("Hb1eta",b1.eta());
-    vars.FillVar("Hb2eta",b2.eta());
-    vars.FillVar("Hb1phi",b1.phi());
-    vars.FillVar("Hb2phi",b2.phi());
-    
-    int idxb1=-1;
-    int idxb2=-1;
-    
-    double minDrB1 = 999;
-    double minDrB2 = 999;
-
-    for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
-      assert(itJetVec->pt()>0);
-      assert(b1.pt()>0);
-      assert(b2.pt()>0);
-      if(tHqUtils::DeltaR(*itJetVec,b1.p4())<minDrB1){
-        idxb1 = itJetVec-jetvecs.begin();
-        minDrB1 = tHqUtils::DeltaR(*itJetVec,b1.p4());
-      }
-      if(tHqUtils::DeltaR(*itJetVec,b2.p4())<minDrB2){
-        idxb2 = itJetVec-jetvecs.begin();
-        minDrB2 = tHqUtils::DeltaR(*itJetVec,b2.p4());
-      }
-    }
-    
-    if(minDrB1<.25){
-      vars.FillVar( "Hb1_idx",idxb1);
-    }
-    if(minDrB2<.25){
-      vars.FillVar( "Hb2_idx",idxb2);
-    }
-  }
-
-  if(sb.pt()>0.){
-    vars.FillVar( "sbpt",sb.pt());
-    vars.FillVar( "sbeta",sb.eta());
-    vars.FillVar( "sbphi",sb.phi());
-    vars.FillVar( "sbm",sb.M());
-  }
-  if(lq.pt()>0.){
-    vars.FillVar("lqpt",lq.pt());
-    vars.FillVar("lqeta",lq.eta());
-    vars.FillVar("lqphi",lq.phi());
-    
-    int idxsb=-1;
-    int idxlq=-1;
-    
-    double minDrSB = 999;
-    double minDrLQ = 999;
-
-    for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
-      assert(itJetVec->pt()>0);
-      assert(sb.pt()>0);
-      assert(lq.pt()>0);
-      if(tHqUtils::DeltaR(*itJetVec,sb.p4())<minDrSB){
-        idxsb = itJetVec-jetvecs.begin();
-        minDrSB = tHqUtils::DeltaR(*itJetVec,sb.p4());
-      }
-      if(tHqUtils::DeltaR(*itJetVec,lq.p4())<minDrLQ){
-        idxLQ = itJetVec-jetvecs.begin();
-        minDrLQ = tHqUtils::DeltaR(*itJetVec,lq.p4());
-      }
-    }
-    
-    if(minDrSB<.25){
-      vars.FillVar( "sb_idx",idxsb);
-    }
-    if(minDrLQ<.25){
-      vars.FillVar( "lq_idx",idxlq);
-    }
-  }
-
+  if(minDrTopLep>4)
+    idxblep=-99;
   
-  std::cout << "IsFilled: "<< input.genTopEvt.IsFilled() << " | TTxIsFilled: " << input.genTopEvt.TTxIsFilled() << " | IsSemiLepton: " << input.genTopEvt.IsSemiLepton() << endl;
-
-  if(input.genTopEvt.IsFilled()&&input.genTopEvt.TTxIsFilled()&&input.genTopEvt.IsSemiLepton()){
-    std::vector<reco::GenJet> bhad_genjet=input.genTopEvt.GetAllTopHadBGenJets();
-    std::vector<reco::GenJet> blep_genjet=input.genTopEvt.GetAllTopLepBGenJets();
-    reco::GenJet b1_genjet=input.genTopEvt.GetHiggsBBarGenJet();
-    reco::GenJet b2_genjet=input.genTopEvt.GetHiggsBGenJet();
-
-    std::vector<reco::GenParticle> bhad_hadron=input.genTopEvt.GetAllTopHadBHadrons();
-    std::vector<reco::GenParticle> blep_hadron=input.genTopEvt.GetAllTopLepBHadrons();
-    reco::GenParticle b1_hadron=input.genTopEvt.GetHiggsBBarHadron();
-    reco::GenParticle b2_hadron=input.genTopEvt.GetHiggsBHadron();
-
-    vars.FillVar( "GenHiggs_B1_GenJet_Pt",b1_genjet.pt() );
-    vars.FillVar( "GenHiggs_B2_GenJet_Pt",b2_genjet.pt() );
-    vars.FillVar( "GenHiggs_B1_GenJet_Eta",b1_genjet.eta() );
-    vars.FillVar( "GenHiggs_B2_GenJet_Eta",b2_genjet.eta());
-    vars.FillVar( "GenHiggs_B1_GenJet_Phi",b1_genjet.phi());
-    vars.FillVar( "GenHiggs_B2_GenJet_Phi",b2_genjet.phi() );
-
-    vars.FillVar( "GenHiggs_B1_Hadron_Pt",b1_hadron.pt() );
-    vars.FillVar( "GenHiggs_B2_Hadron_Pt",b2_hadron.pt() );
-    vars.FillVar( "GenHiggs_B1_Hadron_Eta",b1_hadron.eta() );
-    vars.FillVar( "GenHiggs_B2_Hadron_Eta",b2_hadron.eta());
-    vars.FillVar( "GenHiggs_B1_Hadron_Phi",b1_hadron.phi());
-    vars.FillVar( "GenHiggs_B2_Hadron_Phi",b2_hadron.phi() );
- 
-    for(uint i=0;i<bhad_genjet.size();i++){
-      if(bhad_genjet[i].pt()>1){
-	vars.FillVars( "GenTopHad_B_GenJet_Pt",i,bhad_genjet[i].pt() );
-	vars.FillVars( "GenTopHad_B_GenJet_Eta",i,bhad_genjet[i].eta() );
-	vars.FillVars( "GenTopHad_B_GenJet_Phi",i,bhad_genjet[i].phi());
-      }
-      if(bhad_hadron[i].pt()>1){
-	vars.FillVars( "GenTopHad_B_Hadron_Pt",i,bhad_hadron[i].pt() );
-	vars.FillVars( "GenTopHad_B_Hadron_Eta",i,bhad_hadron[i].eta() );
-	vars.FillVars( "GenTopHad_B_Hadron_Phi",i,bhad_hadron[i].phi());
-      }
-    }
+  
+  vars.FillVar("btop_idx",idxblep);
     
-    for(uint i=0;i<blep_genjet.size();i++){
-      if(blep_genjet[i].pt()>1){
-	vars.FillVars( "GenTopLep_B_GenJet_Phi",i,blep_genjet[i].phi());
-	vars.FillVars( "GenTopLep_B_GenJet_Pt",i,blep_genjet[i].pt() );
-	vars.FillVars( "GenTopLep_B_GenJet_Eta",i,blep_genjet[i].eta());
-      }
-      if(blep_hadron[i].pt()>1){
-	vars.FillVars( "GenTopLep_B_Hadron_Pt",i,blep_hadron[i].pt() );
-	vars.FillVars( "GenTopLep_B_Hadron_Eta",i,blep_hadron[i].eta());
-	vars.FillVars( "GenTopLep_B_Hadron_Phi",i,blep_hadron[i].phi());
-      }
+    //    std::cout << "Found Jet to b from top: #" << idxblep << "  with dR= " << minDrTopLep << endl; 
+  
+  int idxhbb[2];
+  
+  double minDrB1 = 999;
+  double minDrB2 = 999;
+  
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
+    assert(itJetVec->pt()>0);
+    assert(Hdau[0].pt()>0);
+    assert(Hdau[1].pt()>0);
+    if(tHqUtils::DeltaR(*itJetVec,Hdau[0].p4())<minDrB1){
+      idxhbb[0] = itJetVec-jetvecs.begin();
+      minDrB1 = tHqUtils::DeltaR(*itJetVec,Hdau[0].p4());
+    }
+    if(tHqUtils::DeltaR(*itJetVec,Hdau[1].p4())<minDrB2){
+      idxhbb[1] = itJetVec-jetvecs.begin();
+      minDrB2 = tHqUtils::DeltaR(*itJetVec,Hdau[1].p4());
     }
   }
+    
+  if(minDrB1>.4){
+    idxhbb[0]=-99.;
+  }
+  if(minDrB2>.4){
+    idxhbb[1]=-99.;
+  }
+  
+  for (int i =0; i<2; i++)
+    vars.FillVars("hbbjtidx",i,idxhbb[i]);
+
+  int idxsb=-1;
+  int idxlq=-1;
+    
+  double minDrSB = 999;
+  double minDrLQ = 999;
+
+  for(std::vector<math::XYZTLorentzVector>::iterator itJetVec = jetvecs.begin() ; itJetVec != jetvecs.end(); ++itJetVec){
+    assert(itJetVec->pt()>0);
+    assert(sb.pt()>0);
+    assert(lq.pt()>0);
+    if(tHqUtils::DeltaR(*itJetVec,sb.p4())<minDrSB){
+      idxsb = itJetVec-jetvecs.begin();
+      minDrSB = tHqUtils::DeltaR(*itJetVec,sb.p4());
+    }
+    if(tHqUtils::DeltaR(*itJetVec,lq.p4())<minDrLQ){
+      idxlq = itJetVec-jetvecs.begin();
+      minDrLQ = tHqUtils::DeltaR(*itJetVec,lq.p4());
+    }
+  }
+  
+  if(minDrSB>.4)
+    idxsb=-99;
+  
+  vars.FillVar("sbjtidx",idxsb);
+
+  if(minDrLQ>.4)
+    idxlq=-99;
+  
+  vars.FillVar("lqjtidx",idxlq);
+
 }
