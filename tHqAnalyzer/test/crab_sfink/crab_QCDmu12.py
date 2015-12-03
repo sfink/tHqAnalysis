d6d6b871d8646748bbde542b7de3e1f99eb14322
Simon Fink <simon.fink@kit.edu>
2015-12-03 14:15:58 +0100

    moved crab to own folder

---
+++ tHqAnalyzer/test/crab_sfink/crab_QCDmu12.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+import os
+os.environ['GLOBALTAG'] = '74X_mcRun2_asymptotic_v4'
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8'
+config.General.workArea = 'crab_projects'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/desy.de/user/f/fink/xxl/af-cms/13TeV/CMSSW_7_4_15_patch1/src/tHqAnalysis/tHqAnalyzer/test/tHqAnalysis_cfg.py'
+config.JobType.outputFiles = ['tHqAnalyzed_Tree.root']
+
+config.section_("Data")
+config.Data.inputDataset = '/QCD_Pt-1000toInf_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v2/MINIAODSIM'
+                            
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.publication = False
+#config.Data.totalUnits = 5
+#config.Data.publishDbsUrl = 'phys03'
+config.Data.outputDatasetTag = 'tHqAnalysis_MiniAOD'
+
+config.General.transferOutputs = True
+
+config.section_("Site")
+config.Site.storageSite = 'T2_DE_DESY'
---
+++ tHqAnalyzer/test/crab_sfink/crab_el.py
@@ -0,0 +1,30 @@
+from WMCore.Configuration import Configuration
+import os
+os.environ['GLOBALTAG'] = '74X_dataRun2_v5'
+
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = 'SingleElectron_json'
+config.General.workArea = 'crab_projects'
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/desy.de/user/f/fink/xxl/af-cms/13TeV/CMSSW_7_4_15_patch1/src/tHqAnalysis/tHqAnalyzer/test/tHqAnalysis_cfg.py'
+config.JobType.outputFiles = ['tHqAnalyzed_Tree.root']
+
+config.section_("Data")
+config.Data.inputDataset = '/SingleElectron/Run2015D-05Oct2015-v1/MINIAOD'
+                            
+config.Data.inputDBS = 'global'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.publication = False
+#config.Data.totalUnits = 5
+#config.Data.publishDbsUrl = 'phys03'
+config.Data.outputDatasetTag = 'tHqAnalysis_MiniAOD'
+config.Data.lumiMask = '/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-260627_13TeV_PromptReco_Collisions15_25ns_JSON.txt'
+config.General.transferOutputs = True
+
+config.section_("Site")
+config.Site.storageSite = 'T2_DE_DESY'
