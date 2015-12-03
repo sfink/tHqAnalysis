0b752c5385d6b477fc1671e2e5607d35793d7910
Simon Fink <simon.fink@kit.edu>
2015-12-03 14:15:58 +0100

    moved crab to own folder

--- tHqAnalyzer/test/crab_sfink/crab_QCDmu5.py
+++ tHqAnalyzer/test/crab_sfink/crab_QCDmu5.py
@@ -1,21 +1,24 @@
 from WMCore.Configuration import Configuration
+import os
+os.environ['GLOBALTAG'] = '74X_mcRun2_asymptotic_v4'
+
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'WZ_TuneCUETP8M1_13TeV-pythia8'
+config.General.requestName = 'QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = '/afs/desy.de/user/f/fink/xxl/af-cms/13TeV/CMSSW_7_4_2/src/tHqAnalysis/tHqAnalyzer/test/tHqAnalysis_cfg.py'
+config.JobType.psetName = '/afs/desy.de/user/f/fink/xxl/af-cms/13TeV/CMSSW_7_4_15_patch1/src/tHqAnalysis/tHqAnalyzer/test/tHqAnalysis_cfg.py'
 config.JobType.outputFiles = ['tHqAnalyzed_Tree.root']
 
 config.section_("Data")
-config.Data.inputDataset = '/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
-
+config.Data.inputDataset = '/QCD_Pt-80to120_MuEnrichedPt5_TuneCUETP8M1_13TeV_pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
+                            
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 1
+config.Data.unitsPerJob = 5
 config.Data.publication = False
 #config.Data.totalUnits = 5
 #config.Data.publishDbsUrl = 'phys03'
